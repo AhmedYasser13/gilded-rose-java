80298be2d1364f6e2beb335fd0c28650e7dfef69
A.Yasser <ahmed.yasser.asu@gmail.com>
2022-02-25 14:16:36 +0200

    added and refactored congured Test cases

--- src/test/java/com/gildedrose/ConjuredTest.java
+++ src/test/java/com/gildedrose/ConjuredTest.java
@@ -1,23 +1,32 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConjuredTest {
+    GildedRose gildedRose;
+
+    @BeforeEach
+    void beforeAll() {
+        Item[] Conjured = new Item[]{
+                new Item("Conjured Mana Cake", 3, 10),
+                new Item("Conjured Mana Cake", 0, 10),
+                new Item("Conjured Mana Cake", 0, 5),
+        };
+        gildedRose = new GildedRose(Conjured);
+    }
+
     @Test
-    void conjuredItem(){
-        Item[] Conjured = new Item[]{ new Item("Conjured Mana Cake" , 3 ,10) };
-        GildedRose gildedRose= new GildedRose(Conjured);
+    void conjuredItem() {
         assertEquals("Conjured Mana Cake", gildedRose.items[0].name);
         assertEquals(3, gildedRose.items[0].sellIn);
         assertEquals(10, gildedRose.items[0].quality);
     }
 
     @Test
-    void conjuredPriorSellInQuality(){
-        Item[] Conjured = new Item[]{ new Item("Conjured Mana Cake" , 3 ,10) };
-        GildedRose gildedRose= new GildedRose(Conjured);
+    void conjuredPriorSellInQuality() {
         gildedRose.simulateDay();
         assertEquals(8, gildedRose.items[0].quality);
         gildedRose.simulateDay();
@@ -25,13 +34,18 @@ public class ConjuredTest {
     }
 
     @Test
-    void conjuredPostSellInQuality(){
-        Item[] Conjured = new Item[]{ new Item("Conjured Mana Cake" , 0 ,10) };
-        GildedRose gildedRose= new GildedRose(Conjured);
+    void conjuredPostSellInQuality() {
         gildedRose.simulateDay();
-        assertEquals("Conjured Mana Cake", gildedRose.items[0].name);
-        assertEquals(6, gildedRose.items[0].quality);
+        assertEquals(6, gildedRose.items[1].quality);
+        gildedRose.simulateDay();
+        assertEquals(2, gildedRose.items[1].quality);
     }
 
-
+    @Test
+    void conjuredQualityCanNeverBeNegative() {
+        gildedRose.simulateDay();
+        assertEquals(1, gildedRose.items[2].quality);
+        gildedRose.simulateDay();
+        assertEquals(0, gildedRose.items[2].quality);
+    }
 }
